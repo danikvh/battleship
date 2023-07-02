fb598a468d9312a62eaff683ae82cd396d725834
danikvh <danivicen@gmail.com>
2023-07-02 20:00:40 +0200

    Ship

--- src/factories/Ship.js
+++ src/factories/Ship.js
@@ -1,7 +1,19 @@
 export default class Ship {
-    constructor(length) {
+    constructor(length, position) {
         this.length = length
-        this.hits = 0
+        this.position = position
+        this.hits = []
         this.isSunk = false
+    }
+
+    hit(index) {
+        this.hits.push(index)
+        if (this.hits === this.length) {
+            this.isSunk = true
+        }
+    }
+
+    isSunk() {
+        return isSunk
     }
 }
