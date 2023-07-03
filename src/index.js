d43f2dfd724f75eba5ddfc8d3e604fb4d1246fe0
danikvh <danivicen@gmail.com>
2023-07-03 14:34:55 +0200

    Beginning game loop

--- src/index.js
+++ src/index.js
@@ -1,24 +1,95 @@
-//const playBattleships = require('./playBattleships')
+// Import necessary classes and modules
 import './style.css';
+import Ship from "./factories/Ship";
 import Gameboard from "./factories/Gameboard"
+import Player from "./factories/Player"
+import PlayerAI from "./factories/PlayerAI"
 
-// Ejecutar el juego
-//playBattleships();
-const board = new Gameboard()
-const boardElement = document.getElementById('board');
-const boardSize = 10; // Tamaño del tablero (10x10 en este caso)
+// Create players and gameboards
+const playerGameboard = new Gameboard();
+const computerGameboard = new Gameboard();
+const player = new Player(playerGameboard);
+const computer = new PlayerAI(computerGameboard);
 
-// Generar las filas y celdas del tablero
-for (let row = 0; row < boardSize; row++) {
-    const tr = document.createElement('tr');
-    for (let col = 0; col < boardSize; col++) {
-        const td = document.createElement('td');
-        td.dataset.row = row;
-        td.dataset.col = col;
-        tr.appendChild(td);
+// Populate gameboards with predetermined ship coordinates
+const playerShip1 = new Ship(3, [[3,2],[3,3],[3,4]])
+const playerShip2 = new Ship(4, [[2,2],[2,3],[2,4],[2,5]])
+const playerShip3 = new Ship(4, [[6,2],[6,3],[6,4],[6,5]])
+const playerShip4 = new Ship(5, [[5,3],[5,4],[5,5],[5,6],[5,7]])
+playerGameboard.placeShip(playerShip1)
+playerGameboard.placeShip(playerShip2)
+playerGameboard.placeShip(playerShip3)
+playerGameboard.placeShip(playerShip4)
+
+const computerShip1 = new Ship(3, [[3,2],[3,3],[3,4]])
+const computerShip2 = new Ship(4, [[2,2],[2,3],[2,4],[2,5]])
+const computerShip3 = new Ship(4, [[6,2],[6,3],[6,4],[6,5]])
+const computerShip4 = new Ship(5, [[5,3],[5,4],[5,5],[5,6],[5,7]])
+computerGameboard.placeShip(computerShip1)
+computerGameboard.placeShip(computerShip2)
+computerGameboard.placeShip(computerShip3)
+computerGameboard.placeShip(computerShip4)
+
+
+// Render the gameboards on the UI
+// Generate the gameboard HTML for the player
+const playerBoard = document.getElementById('player-board');
+generateGameboardHTML(playerBoard, playerGameboard);
+
+// Generate the gameboard HTML for the AI
+const aiBoard = document.getElementById('ai-board');
+generateGameboardHTML(aiBoard, computerGameboard);
+
+// Function to generate the gameboard HTML
+function generateGameboardHTML(boardElement, gameboard) {
+    for (let i = 0; i < 10; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 10; j++) {
+            const cell = document.createElement('td');
+            // Check if the cell has a ship and set the appropriate CSS class
+            if (gameboard.board[i][j].hasShip) {
+                cell.classList.add('ship');
+            } else {
+                cell.classList.add('water');
+            }
+            row.appendChild(cell);
+        }
+        boardElement.querySelector('tbody').appendChild(row);
     }
-    boardElement.querySelector('tbody').appendChild(tr);
 }
 
 
-console.log(board)
+// Add event listeners for attacking
+
+// Implement the game loop
+while (!gameOver()) {
+    // Player's turn
+    const playerAttackCoords = getPlayerAttackCoords(); // Get attack coordinates from user input
+    const playerAttackResult = computerGameboard.receiveAttack(playerAttackCoords);
+    // Update UI to reflect the attack result
+
+    // Computer's turn
+    const computerAttackCoords = getComputerAttackCoords(); // Generate random attack coordinates
+    const computerAttackResult = playerGameboard.receiveAttack(computerAttackCoords);
+    // Update UI to reflect the attack result
+}
+
+// Function to check if the game is over
+function gameOver() {
+    // Check if any player's ships have all been sunk
+    // Return true if game over, false otherwise
+}
+
+// Function to handle player's attack input
+function getPlayerAttackCoords() {
+    // Logic to capture player's attack coordinates from user input
+    // Return the attack coordinates
+}
+
+// Function to generate computer's attack coordinates
+function getComputerAttackCoords() {
+    // Logic to generate random attack coordinates for the computer
+    // Return the attack coordinates
+}
+
+// Other helper functions and code
